3c078d66c47dff1a27b36718ad3d39dc0d27a975
Daniel Mota <AndroidKing@gmail.com>
2017-05-27 18:16:29 -0400

    Adding undirect based on requests, user agent switching and sleeping random amounts of time between requests.

--- urlRedirect.py
+++ urlRedirect.py
@@ -1,6 +1,11 @@
 from urllib import request, error
 import csv
 import sys
+import random
+from time import sleep
+import requests
+
+user_agents = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36", "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.1 Safari/603.1.30", "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"]
 
 def undirect(urlsFile):
     """Passed in a csv file of urls, it returns a list of what they rediect to."""
@@ -10,25 +15,44 @@ def undirect(urlsFile):
         for url in fileReader:
             try:
                 opener = request.build_opener(request.HTTPCookieProcessor)
-                opener.addheaders = [('User-Agent', "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0")]
-                yield (url[0], url[1], opener.open(url[1]).geturl())
-            except error.HTTPError as e:
-                print(e)
-                if "linkedin" in e.headers.get("Set-Cookie"):
-                    print("LinkedIn")
-                print("{0} {1}\n".format(url[0], url[1]))
-                yield (url[0], url[1])
-            except ValueError as e:
+                opener.addheaders = [('User-Agent', random.choice(user_agents))]
+                request.install_opener(opener)
+                
+                print(".", end='')
+                yield (url[0], url[1], request.urlopen(url[1]).geturl())
+                sleep(random.random())
+            except Exception as e: #(error.HTTPError, error.URLError, ValueError) as e:
                 print(e)
                 print("{0} {1}\n".format(url[0], url[1]))
                 yield (url[0], url[1])
     
     
-if __name__ == "__main__":
+def undirect_w_requests(urlsFile):
+    """Passed in a csv file of urls, it returns a list of what they rediect to. Uses requests instead of urllib."""
+    with open(urlsFile, 'r', newline='') as urls:
+        fileReader = csv.reader(urls)
+        
+        for url in fileReader:
+            try:
+                print(".", end='')
+                yield (url[0], url[1], requests.get(url[1]).url)
+                sleep(random.random())
+            except Exception as e: #(error.HTTPError, error.URLError, ValueError) as e:
+                print(e)
+                print("{0} {1}\n".format(url[0], url[1]))
+                yield (url[0], url[1])
+    
+    
+def main():
     file_name = "./dirtyUrls.csv"
     pureurls = undirect(file_name)
     
     with open('cleanUrls.csv', 'w', newline='') as f:
         fileWriter = csv.writer(f)
-        fileWriter.writerows(pureurls)
+        for row in pureurls:
+            fileWriter.writerow(row)
+    
+    
+if __name__ == "__main__":
+    main()
         
